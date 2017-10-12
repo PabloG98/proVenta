7b8dde75c85a4ab58e632bc0b43ea866f9059f4b
Pablo David García Chocón <pgarcia@apde.edu.gt>
2017-10-12 15:29:55 -0600

    actualizaciones

--- views/facturacion.php
+++ views/facturacion.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>proVenta - Inicio</title>
+        <title>proVenta - Facturación</title>
         <!-- Bootstrap core CSS -->
         <link href="../css/bootstrap.min.css" rel="stylesheet">
 
@@ -40,7 +40,7 @@ and open the template in the editor.
                 </div>
                 <div id="navbar" class="navbar-collapse collapse navbar-ex1-collapse">
                     <ul class="nav navbar-nav navbar-right">
-                        <li><a style="color: green" onclick="logout(this.form)"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+                        <li><a style="color: green"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
                     </ul>
                 </div>
             </div>
@@ -50,18 +50,14 @@ and open the template in the editor.
                 <div class="container">
                     <div class="col-sm-3 col-md-2 sidebar" style="background: greenyellow">
                         <ul class="nav nav-sidebar">
-                            <li class="active"><a style="color: white"><span class="glyphicon glyphicon-list-alt"></span> Inventario</a></li>
-                            <li><a href="facturacion.php" style="color: green"><span class="glyphicon glyphicon-usd"></span> Facturar</a></li>
+                            <li><a href="inicio.php" style="color: green"><span class="glyphicon glyphicon-list-alt"></span> Inventario</a></li>
+                            <li class="active"><a href="" style="color: white"><span class="glyphicon glyphicon-usd"></span> Facturar</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="container">
                     <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2">
-                        <h2 class="text-center">Control de Inventario</h2><hr>
-                        <div class="text-center alert alert-success alert-dismissable">
-                            <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
-                            <strong>¡BIENVENIDO!</strong><br> Inicio de sesión con éxito.
-                        </div>
+                        <h2 class="text-center">Facturación</h2><hr>
                     </div>
                 </div>
             </div>
@@ -69,22 +65,5 @@ and open the template in the editor.
         <?php
         // put your code here
         ?>
-        <!-- Bootstrap core JavaScript
-        ================================================== -->
-        <!-- Placed at the end of the document so the pages load faster -->
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-        <script src="../../dist/js/bootstrap.min.js"></script>
-        <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-        <script src="../../assets/js/vendor/holder.min.js"></script>
-        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script> 
-        <script src="js/jquery.min.js"></script>
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript">
-                            function logout(form) {
-                                window.location.replace("../index.php");
-                            }
-        </script>
     </body>
 </html>
